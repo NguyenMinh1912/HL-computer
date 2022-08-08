a0472ff80eb9f91a2e5cebec22828ad4a6581f1a
minhsama2000 <minhsama2000@gmail.com>
2022-08-08 22:44:14 +0700

    add antd, admin ui

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,16 +1,81 @@
-import { FunctionComponent } from "react";
-import { Outlet } from "react-router-dom";
-import { Footer } from "./common/Footer";
-import { Header } from "./common/Header";
+import React, {FunctionComponent, useState} from "react";
+import "antd/dist/antd.css";
+import "./index.css";
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    ContainerOutlined,
+    ShoppingCartOutlined,
+    BarChartOutlined
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+
+const { Header, Sider, Content } = Layout;
 
 const Admin: FunctionComponent = () => {
-  return <div>
-    <Header></Header>
+    const [collapsed, setCollapsed] = useState(false);
 
-    <Outlet/>
+    const sideBarRouter = () => {
+        console.log("minhsama")
+    }
 
-    <Footer></Footer>
-  </div>;
+    return (
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={["1"]}
+                    items={[
+                        {
+                            key: "1",
+                            icon: <BarChartOutlined />,
+                            label: "DashBoard",
+                            onClick: sideBarRouter
+                        },
+                        {
+                            key: "2",
+                            icon: <ShoppingCartOutlined />,
+                            label: "Product",
+
+                        },
+                        {
+                            key: "3",
+                            icon: <ContainerOutlined />,
+                            label: "Category"
+                        }
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0
+                    }}
+                >
+                    {React.createElement(
+                        collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                        {
+                            className: "trigger",
+                            onClick: () => setCollapsed(!collapsed)
+                        }
+                    )}
+                </Header>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: "24px 16px",
+                        padding: 24,
+                        minHeight: 1000
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    );
 };
 
-export { Admin };
+export {Admin};
