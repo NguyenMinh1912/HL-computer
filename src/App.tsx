b465b5cdf4ad2ec9cac79920dbecdc0a8fc59071
minhnc <ncm292@gmail.com>
2022-08-09 23:21:12 +0700

    Add Private Route And fix Admin Layout UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
 import React, {useEffect} from "react";
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
-import { User } from "./pages/User";
-import { Admin } from "./pages/Admin";
-import { NotFound } from "./common/NotFound";
-import { Products } from "./pages/Admin/components/Products";
-import { Categories } from "./pages/Admin/components/Categories";
+import {Link, Route, Routes} from "react-router-dom";
+import {User} from "./pages/User";
+import {Admin} from "./pages/Admin";
+import {NotFound} from "./common/NotFound";
+import {Products} from "./pages/Admin/components/Products";
+import {Categories} from "./pages/Admin/components/Categories";
 import {DashBoard} from "./pages/Admin/components/DashBoard";
 import authenticateService from "./core/services/AuthenticateService";
 import Login from "./pages/Login/Login";
+import {PrivateRoute} from "./common/PrivateRoute/PrivateRoute";
+import {Simulate} from "react-dom/test-utils";
 
 function App() {
 
 
-
-  return (
-    <div className="App">
-      {/* <Link to={''}>
+    return (
+        <div className="App">
+            {/* <Link to={''}>
         User page
       </Link>
       <Link to={'admin'}>
         Admin page
       </Link> */}
-        <Routes>
-          {/*<Route path="" element={<User />}>*/}
-          {/*  /!* user childs route here *!/*/}
-          {/*</Route>*/}
-          <Route path="" element={<Admin />}>
-            {/* admin childs route here */}
-            <Route path="products" element={<Products/>}></Route>
-            <Route path="categories" element={<Categories/>}></Route>
-            <Route path="dashboard" element={<DashBoard/>}></Route>
-          </Route>
-          <Route path="login" element={<Login/>}></Route>
-          <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
-    </div>
-  );
+            <Routes>
+                {/*<Route path="" element={<User />}>*/}
+                {/*  /!* user childs route here *!/*/}
+                {/*</Route>*/}
+                <Route path="" element={<PrivateRoute isGuard={authenticateService.isAdmin()} >
+                {/*<Route path="" element={<PrivateRoute isGuard={true} >*/}
+                    <Admin/>
+                </PrivateRoute>}>
+                    {/* admin childs route here */}
+                    <Route path="products" element={<Products/>}></Route>
+                    <Route path="categories" element={<Categories/>}></Route>
+                    <Route path="dashboard" element={<DashBoard/>}></Route>
+                </Route>
+                <Route path="login" element={<Login/>}></Route>
+                <Route path="*" element={<NotFound/>}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/common/PrivateRoute/PrivateRoute.tsx
+++ src/common/PrivateRoute/PrivateRoute.tsx
@@ -1,12 +1,16 @@
-import {FunctionComponent} from "react";
+import {FunctionComponent, ReactElement} from "react";
 import {Navigate, Outlet} from "react-router-dom";
 import authenticateService from "../../core/services/AuthenticateService";
 import Login from "../../pages/Login/Login";
 
-// @ts-ignore
-const PrivateRoute: FunctionComponent = ({ auth: { isAuthenticated }, children }) => {
-    let isLogin = authenticateService.isLogin();
-    return isLogin ? children : <Navigate to="/login"/>
+type PrivateRouteProps = {
+    isGuard: boolean;
+    children: ReactElement
+}
+
+const PrivateRoute: FunctionComponent<PrivateRouteProps> = ({isGuard, children}) => {
+    // let isLogin = authenticateService.isLogin();
+    return isGuard ? children : <Navigate to="/login"/>
 }
 
 export {PrivateRoute};
